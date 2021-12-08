511feb6938d84cff98f356d466e077d705efee9c
bymse <egorstrelov@gmail.com>
2021-12-08 22:21:17 +0300

    Lab 1 refactoring

--- lab1.m
+++ lab1.m
@@ -1,36 +1,47 @@
-P = [1, 1; 0, 1];
-Q = [0;1];
+syms Q(t)
+P = [0, 1; 0, 0];
+Q = [t 0; 0 1];
 f = [0;0];
 T = 1;
-x_0 = [0;0];
-x_1 = [1;1];
+x_0 = [1; 1];
+x_1 = [0;1];
 
-can_be = can_be_controlled(Q, P, length(x_0));
-disp('Критерий Калмана:');
-if can_be
-    disp('выполняется');
-else
-    disp('не выполняется');
+if kalman_condition_met(Q, P, length(x_0))
+    fprintf("критерий Калмана выполняется\n");
+else 
+    fprintf("критерий Калмана не выполняется\n");
 end
+
 syms Y(t)
-Y(t) = expm(P*t);
+Y = expm(P*t);
+fprintf("фундаментальная матрица: %s\n", char(Y));
+
 syms B(t)
-B(t) = (Y(t)^-1)*Q;
-A = int(B * B.', 0, T);
-eta = (Y(T)^-1)*x_1 - x_0 - int((Y(t)^-1) * f, 0, T);
-C = (A^-1)*eta;
-u = B'*C;
-disp('u(t):');
+B = (Y^-1)*Q;
+fprintf("B(t): %s\n", char(B));
+
+
+A = int(B*B.', 0, T); %'
+fprintf("A: %s:\n", char(A));
+
+eta = subs(Y^(-1)*x_1-x_0-int(Y^(-1)*f,0, T), T);
+fprintf("eta: %s\n", char(eta));
+
+C = A^(-1)*eta;
+u = (B.'* C); %'
+
+fprintf("u(t): \n");
 disp(u);
 
-
-function can_be = can_be_controlled(Q, P, n)
+function met = kalman_condition_met(Q, P, n)
     matrix = Q;
-    counter = 0;
-    while counter < n
-        mult = P^counter * Q;
-        matrix = [matrix mult];
-        counter = counter + 1;
+
+    for c = 1:n
+        m = P^c * Q;
+        matrix = [matrix m];
+        c = c + 1;
     end
-    can_be = rank(matrix) == n;
+    r = rank(matrix);
+    fprintf("rank %s = %d\n", char(matrix), r);
+    met = r == n;
 end
